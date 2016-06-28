2d912dea838dfda1c6e36326c27fc2b538655a48
Quoc Vu <darthvu@gmail.com>
2016-06-28 14:47:04 -0700

    setup aws resources via cloud formation

---
+++ src/monitor.js
@@ -0,0 +1,13 @@
+/*
+- check a site
+- if success & error_mode => send 'back online' sms
+- if fail => send & error_count < max => send failure sms and incr(error_count)
+*/
+
+'use strict';
+
+var exports = module.exports = {
+  var handler = function () {
+    console.log('here');
+  }
+};
